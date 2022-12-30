5faf0ff080bb7d37d5fc80827a41dffaca69d3b2
MALEKMFM <malekmshensh@gmail.com>
2022-12-30 19:52:39 +0100

    Add files via upload

---
+++ Project.4/main.js
@@ -0,0 +1,9 @@
+let image = document.querySelector('.image');
+let backgroundColor = document.querySelector('.container');
+function phones(phone){
+    image.src = phone;
+}
+
+function colors(color){
+    backgroundColor.style.background = color;
+}
